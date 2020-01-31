4155d716af891e522e50195fe179e1bf8a0b5d51
Katie McConkey <2366393m@student.gla.ac.uk>
2020-01-31 14:25:21 +0000

    updated urls

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('rango/', include('rango.urls')),
     path('admin/', admin.site.urls),
+    ##path('about/', include('about.urls'))
 ]
