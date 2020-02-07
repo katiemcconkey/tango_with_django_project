6304bebb6d41d76f3003b919bc48762234cee1ae
Katie McConkey <2366393m@student.gla.ac.uk>
2020-02-07 14:39:59 +0000

    Chapter 6

--- rango/views.py
+++ rango/views.py
@@ -8,6 +8,8 @@ def index(request):
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
     return render(request, 'rango/index.html', context=context_dict)
 
 ##    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
